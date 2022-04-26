2ee7781bc5a0637f5165c67259d01ff19ccbf953
José Rodrigues <67165102+souma458@users.noreply.github.com>
2022-04-26 21:51:26 +0100

    Add database setup

---
+++ database/mongodb/setup-db.js
@@ -0,0 +1,16 @@
+print("Starting the process of populating databases");
+
+const databases = {
+  thirdPartyTransportationDatabase: "third_party_transportation_db",
+  packageDatabase: "package_db",
+  deliveryDatabase: "delivery_db",
+};
+
+function createTransportationRequests() {
+  // TODO: Create some transportation requests (if needed)
+  print(`Setting up transportation requests`);
+}
+
+createTransportationRequests();
+
+print("Populating databases has ended");
---
+++ database/mysql/setup-db.sql
@@ -0,0 +1,100 @@
+-- Database creation
+
+CREATE DATABASE IF NOT EXISTS user_db;
+CREATE DATABASE IF NOT EXISTS drone_db;
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+-- User Management tables
+
+USE user_db;
+
+CREATE TABLE personal_info (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    birth_date DATE,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(32),
+    address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE account (
+    id INT AUTO_INCREMENT NOT NULL,
+    personal_info_id INT NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    status VARCHAR(64) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE account ADD CONSTRAINT FK_PersonalInfoAccount FOREIGN KEY (personal_info_id) REFERENCES personal_info(id);
+ALTER TABLE account ADD CONSTRAINT UC_AccountUsername UNIQUE(username);
+
+-- Drone Management tables
+
+USE drone_db;
+
+CREATE TABLE drone (
+    id INT AUTO_INCREMENT NOT NULL,
+    model VARCHAR(64) NOT NULL,
+    color VARCHAR(64) NOT NULL,
+    status VARCHAR(64) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE model (
+    id VARCHAR(64) NOT NULL,
+    weight INT NOT NULL, 
+    height INT NOT NULL,
+    width INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE drone ADD CONSTRAINT FK_DroneModel FOREIGN KEY (model) REFERENCES model(id);
+ALTER TABLE model ADD CONSTRAINT weight_greater_than_zero CHECK (weight > 0);
+ALTER TABLE model ADD CONSTRAINT height_greater_than_zero CHECK (height > 0);
+ALTER TABLE model ADD CONSTRAINT width_greater_than_zero CHECK (width > 0);
+
+-- Setup User Management data
+
+USE user_db;
+
+INSERT INTO personal_info (first_name, last_name, birth_date, email, phone_number, address) 
+    VALUES("Waverly", "Jones", '1970-02-06', "waverly.jones@email.com", "1747542670", "196 Bay Street Sykesville, MD 21784");
+INSERT INTO personal_info (first_name, last_name, birth_date, email, phone_number, address)
+    VALUES("Merritt", "Mclean", '1979-12-24', "merritt.mclean@email.com", "0867789093", "422 South Division St. Mchenry, IL 60050");
+INSERT INTO personal_info (first_name, last_name, birth_date, email, phone_number, address)
+    VALUES("Budd", "Heath", '1983-01-31', "budd.heath@email.com", "5532001455", "21 South Cypress Dr. South Ozone Park, NY 11420");
+INSERT INTO personal_info (first_name, last_name, birth_date, email, phone_number, address)
+    VALUES("Dana", "Harris", '1984-04-04', "dana.harris@email.com", "7353801376", "275 Pilgrim Ave. Billerica, MA 01821");
+INSERT INTO personal_info (first_name, last_name, birth_date, email, phone_number, address)
+    VALUES("Nellie", "Miles", '1988-03-12', "nellie.miles@email.com", "7430601668", "8305 Ridgeview Street Camas, WA 98607");
+
+INSERT INTO account (personal_info_id, username, password, status) VALUES(1, "wjones", "wjones", "ACTIVE");
+INSERT INTO account (personal_info_id, username, password, status) VALUES(2, "mmclean", "mmclean", "ACTIVE");
+INSERT INTO account (personal_info_id, username, password, status) VALUES(3, "bheath", "bheath", "ACTIVE");
+INSERT INTO account (personal_info_id, username, password, status) VALUES(4, "dharris", "dharris", "ACTIVE");
+INSERT INTO account (personal_info_id, username, password, status) VALUES(5, "nmiles", "nmiles", "ACTIVE");
+
+-- Setup Drone Management data
+
+USE drone_db;
+
+INSERT INTO model (id, weight, height, width) VALUES("DJI Air 2S", 1500, 35, 40);
+INSERT INTO model (id, weight, height, width) VALUES("DJI Mini 2", 2000, 40, 38);
+INSERT INTO model (id, weight, height, width) VALUES("DJI Mavic 2 Pro", 2200, 40, 40);
+INSERT INTO model (id, weight, height, width) VALUES("Parrot Anafi", 1200, 25, 30);
+INSERT INTO model (id, weight, height, width) VALUES("3DR Solo", 1700, 35, 45);
+
+INSERT INTO drone (model, color, status) VALUES("DJI Air 2S", "WHITE", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("DJI Air 2S", "BLUE", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("DJI Mini 2", "GREEN", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("DJI Mini 2", "RED", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("DJI Mavic 2 Pro", "ORANGE", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("DJI Mavic 2 Pro", "YELLOW", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("Parrot Anafi", "PURPLE", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("Parrot Anafi", "BLACK", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("3DR Solo", "GREY", "ACTIVE");
+INSERT INTO drone (model, color, status) VALUES("3DR Solo", "GREY", "ACTIVE");
